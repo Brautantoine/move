a6bb063cafe54537ec556fc822159527ff5c8340
Antoine Braut <antoine.braut@ac6.fr>
2018-10-30 16:08:46 +0100

    Add License lines
    License and contact added to move.sh

--- move.sh
+++ move.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+#########################################################################################
+# <A short bash script to pack all the files in current directory to a new directory>   #
+#    Copyright (C) 2018  Antoine Braut						        #
+#										        #
+#    This program is free software: you can redistribute it and/or modify		#
+#    it under the terms of the GNU General Public License as published by		#
+#    the Free Software Foundation, either version 3 of the License, or			#
+#    (at your option) any later version.						#
+#											#
+#    This program is distributed in the hope that it will be useful,			#
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of			#
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			#
+#    GNU General Public License for more details.					#
+#											#
+#    You should have received a copy of the GNU General Public License			#
+# along with this program. If not, see <https://www.gnu.org/licenses/>.			#
+#											#
+# Contact : Antoine.braut@gmail.com							#
+#											#
+#########################################################################################
+
+
 if [ -z $1 ]; then
 	echo "invalid no arg invoke"
 	exit 1
