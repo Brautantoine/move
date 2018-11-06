21d23d58482cb09433d7797d2641382708bca3ea
Antoine Braut <antoine.braut@ac6.fr>
2018-11-06 14:56:23 +0100

    Adding arg analyse
    --version

--- move.sh
+++ move.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #########################################################################################
-# <A short bash script to pack all the files in current directory to a new directory>   #
+# A short bash script to pack all the files in current directory to a new directory     #
 #    Copyright (C) 2018  Antoine Braut						        #
 #										        #
 #    This program is free software: you can redistribute it and/or modify		#
@@ -23,11 +23,46 @@
 
 display_help()
 {
-	echo "move - version 0.0 (beta)"
-	echo "A short script utils to pack all the file in current directory to a new directory"
-	echo "Usage : move <dest_dir>"
+	echo "move - version 0.0 (beta)
+A short script utils to pack all the files in current directory
+into a new directory
+
+	Usage : move <dest_dir>"
 }
 
+opt_help=0
+opt_source=0
+
+while getopts "hHs-:" opt ; do
+	case $opt in
+		h ) 
+			display_help 
+			exit 0 ;;
+		H ) 
+			display_help 
+			exit 0 ;;
+		s )
+			echo source
+			exit 0 ;;
+		- ) case $OPTARG in
+			help ) 
+				display_help
+				exit 0 ;;
+			source )
+				echo source
+				exit 0 ;;
+			version )
+				echo 0.2 
+				#exit 0 ;;
+;;
+			* )
+				echo "Unrecognized option : --$OPTARG"
+				exit 1 ;;
+			esac ;;
+	esac
+done
+shift $(($OPTIND-1))
+			
 if [ -z $1 ]; then
 	echo "invalid no arg invoke"
 	echo " "
@@ -35,14 +70,14 @@ if [ -z $1 ]; then
 	exit 1
 fi
 
-if [[ "$1" == "--help" || "$1" == "-h" ]]; then
-	display_help
-	exit 0
-fi
+#if [[ "$1" == "--help" || "$1" == "-h" ]]; then
+#	display_help
+#	exit 0
+#fi
 
 if [ ! -d $1 ]; then
 	echo "creating $1"
-	mkdir -- $1
+	mkdir -p -- $1
 fi
 for item in  ./*
 do
